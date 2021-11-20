48eda8065d4643f52f06868ea7a2db321b81b13d
Tadukoo <realtadukoo@gmail.com>
2021-11-20 14:14:22 -0500

    Added SQLStatement
    - SQLStatement is a builder for any SQL Statement ultimately
    - Part of Issue #6

---
+++ TadukooMySQL/junit/com/github/tadukoo/database/mysql/syntax/SQLStatementTest.java
@@ -0,0 +1,87 @@
+package com.github.tadukoo.database.mysql.syntax;
+
+import com.github.tadukoo.database.mysql.syntax.conditional.EqualsStatement;
+import com.github.tadukoo.database.mysql.syntax.reference.ColumnRef;
+import com.github.tadukoo.database.mysql.syntax.reference.TableRef;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SQLStatementTest{
+	private final TableRef table = TableRef.builder()
+			.tableName("Test")
+			.build();
+	private final ColumnRef column = ColumnRef.builder()
+			.columnName("Derp")
+			.build();
+	
+	@Test
+	public void testSelectBuilder(){
+		SQLSelectStatement selectStmt = SQLStatement.builder()
+				.select()
+				.fromTables(table)
+				.build();
+		assertNotNull(selectStmt);
+	}
+	
+	@Test
+	public void testInsertBuilder(){
+		SQLInsertStatement insertStmt = SQLStatement.builder()
+				.insert()
+				.table(table)
+				.values(42)
+				.build();
+		assertNotNull(insertStmt);
+	}
+	
+	@Test
+	public void testUpdateBuilder(){
+		SQLUpdateStatement updateStmt = SQLStatement.builder()
+				.update()
+				.table(table)
+				.setStatements(new EqualsStatement(column, 42))
+				.build();
+		assertNotNull(updateStmt);
+	}
+	
+	@Test
+	public void testDeleteBuilder(){
+		SQLDeleteStatement deleteStmt = SQLStatement.builder()
+				.delete()
+				.table(table)
+				.build();
+		assertNotNull(deleteStmt);
+	}
+	
+	@Test
+	public void testCreateBuilder(){
+		SQLCreateStatement createStmt = SQLStatement.builder()
+				.create()
+				.database()
+				.databaseName("Test")
+				.build();
+		assertNotNull(createStmt);
+	}
+	
+	@Test
+	public void testDropBuilder(){
+		SQLDropStatement dropStmt = SQLStatement.builder()
+				.drop()
+				.database()
+				.name("Test")
+				.build();
+		assertNotNull(dropStmt);
+	}
+	
+	@Test
+	public void testAlterBuilder(){
+		SQLAlterStatement alterStmt = SQLStatement.builder()
+				.alter()
+				.table()
+				.tableName("Test")
+				.drop()
+				.columnName("Derp")
+				.build();
+		assertNotNull(alterStmt);
+	}
+}
---
+++ TadukooMySQL/src/com/github/tadukoo/database/mysql/syntax/SQLStatement.java
@@ -0,0 +1,124 @@
+package com.github.tadukoo.database.mysql.syntax;
+
+/**
+ * SQL Statement is a class used to create a MySQL Statement
+ *
+ * @author Logan Ferree (Tadukoo)
+ * @version Alpha v.0.3
+ */
+public class SQLStatement{
+	
+	/**
+	 * A builder class used to start building a SQL Statement. The following options are available:
+	 *
+	 * <table>
+	 *     <caption>SQL Statement Types</caption>
+	 *     <tr>
+	 *         <th>Type</th>
+	 *         <th>Class</th>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>select</td>
+	 *         <td>{@link SQLSelectStatement}</td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>insert</td>
+	 *         <td>{@link SQLInsertStatement}</td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>update</td>
+	 *         <td>{@link SQLUpdateStatement}</td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>delete</td>
+	 *         <td>{@link SQLDeleteStatement}</td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>create</td>
+	 *         <td>{@link SQLCreateStatement}</td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>drop</td>
+	 *         <td>{@link SQLDropStatement}</td>
+	 *     </tr>
+	 *     <tr>
+	 *         <td>alter</td>
+	 *         <td>{@link SQLAlterStatement}</td>
+	 *     </tr>
+	 * </table>
+	 *
+	 * @author Logan Ferree (Tadukoo)
+	 * @version Alpha v.0.3
+	 */
+	public static class SQLStatementBuilder{
+		
+		/** Not allowed to instantiate outside {@link SQLStatement} */
+		private SQLStatementBuilder(){ }
+		
+		/**
+		 * @return A {@link SQLSelectStatement.SQLSelectStatementBuilder builder} to use to make a
+		 * {@link SQLSelectStatement}
+		 */
+		public SQLSelectStatement.ColumnsAndTables select(){
+			return SQLSelectStatement.builder();
+		}
+		
+		/**
+		 * @return A {@link SQLInsertStatement.SQLInsertStatementBuilder builder} to use to make a
+		 * {@link SQLInsertStatement}
+		 */
+		public SQLInsertStatement.Table insert(){
+			return SQLInsertStatement.builder();
+		}
+		
+		/**
+		 * @return A {@link SQLUpdateStatement.SQLUpdateStatementBuilder builder} to use to make a
+		 * {@link SQLUpdateStatement}
+		 */
+		public SQLUpdateStatement.Table update(){
+			return SQLUpdateStatement.builder();
+		}
+		
+		/**
+		 * @return A {@link SQLDeleteStatement.SQLDeleteStatementBuilder builder} to use to make a
+		 * {@link SQLDeleteStatement}
+		 */
+		public SQLDeleteStatement.Table delete(){
+			return SQLDeleteStatement.builder();
+		}
+		
+		/**
+		 * @return A {@link SQLCreateStatement.SQLCreateStatementBuilder builder} to use to create a
+		 * {@link SQLCreateStatement}
+		 */
+		public SQLCreateStatement.Type create(){
+			return SQLCreateStatement.builder();
+		}
+		
+		/**
+		 * @return A {@link SQLDropStatement.SQLDropStatementBuilder builder} to use to create a
+		 * {@link SQLDropStatement}
+		 */
+		public SQLDropStatement.Type drop(){
+			return SQLDropStatement.builder();
+		}
+		
+		/**
+		 * @return A {@link SQLAlterStatement.SQLAlterStatementBuilder builder} to use to create a
+		 * {@link SQLAlterStatement}
+		 */
+		public SQLAlterStatement.Type alter(){
+			return SQLAlterStatement.builder();
+		}
+	}
+	
+	/** Not allowed to actually instantiate */
+	private SQLStatement(){ }
+	
+	/**
+	 * @return A {@link SQLStatementBuilder builder} to start building a SQL Statement
+	 */
+	public static SQLStatementBuilder builder(){
+		return new SQLStatementBuilder();
+	}
+}
