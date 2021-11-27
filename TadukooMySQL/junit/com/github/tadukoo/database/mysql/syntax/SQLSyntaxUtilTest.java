686dd9b735f7e0077e723f8842ebc8e39cbfc9ca
Tadukoo <realtadukoo@gmail.com>
2021-11-27 14:31:19 -0500

    SQLSyntaxUtil now has makeColumnRef, makeConditionalStatement, and formatInsertStatement
    - Part of Issue #6

--- TadukooMySQL/junit/com/github/tadukoo/database/mysql/syntax/SQLSyntaxUtilTest.java
+++ TadukooMySQL/junit/com/github/tadukoo/database/mysql/syntax/SQLSyntaxUtilTest.java
@@ -1,9 +1,16 @@
 package com.github.tadukoo.database.mysql.syntax;
 
+import com.github.tadukoo.database.mysql.syntax.conditional.ConditionalStatement;
+import com.github.tadukoo.database.mysql.syntax.conditional.SQLOperator;
+import com.github.tadukoo.database.mysql.syntax.reference.ColumnRef;
 import com.github.tadukoo.database.mysql.syntax.reference.TableRef;
+import com.github.tadukoo.util.ListUtil;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class SQLSyntaxUtilTest{
 	
@@ -26,5 +33,90 @@ public class SQLSyntaxUtilTest{
 	public void testConvertValueToStringWithTableRef(){
 		TableRef table = TableRef.builder().tableName("Derp").build();
 		assertEquals("Derp", SQLSyntaxUtil.convertValueToString(table));
+	}
+	
+	@Test
+	public void testMakeColumnRef(){
+		ColumnRef columnRef = SQLSyntaxUtil.makeColumnRef("Derp");
+		assertNull(columnRef.getTableName());
+		assertEquals("Derp", columnRef.getColumnName());
+		assertNull(columnRef.getAlias());
+		assertEquals("Derp", columnRef.toString());
+	}
+	
+	@Test
+	public void testMakeColumnRefDot(){
+		ColumnRef columnRef = SQLSyntaxUtil.makeColumnRef("Derp.Plop");
+		assertEquals("Derp", columnRef.getTableName());
+		assertEquals("Plop", columnRef.getColumnName());
+		assertNull(columnRef.getAlias());
+		assertEquals("Derp.Plop", columnRef.toString());
+	}
+	
+	@Test
+	public void testMakeColumnRefs(){
+		List<ColumnRef> columnRefs = SQLSyntaxUtil.makeColumnRefs(ListUtil.createList("Test", "Derp.Plop"));
+		assertEquals(2, columnRefs.size());
+		ColumnRef columnRef1 = columnRefs.get(0);
+		assertNull(columnRef1.getTableName());
+		assertEquals("Test", columnRef1.getColumnName());
+		assertNull(columnRef1.getAlias());
+		assertEquals("Test", columnRef1.toString());
+		ColumnRef columnRef2 = columnRefs.get(1);
+		assertEquals("Derp", columnRef2.getTableName());
+		assertEquals("Plop", columnRef2.getColumnName());
+		assertNull(columnRef2.getAlias());
+		assertEquals("Derp.Plop", columnRef2.toString());
+	}
+	
+	@Test
+	public void testMakeConditionalStatement(){
+		ConditionalStatement stmt = SQLSyntaxUtil.makeConditionalStmt(false, "Test", 42);
+		ColumnRef column = stmt.getColumn();
+		assertNull(column.getTableName());
+		assertEquals("Test", column.getColumnName());
+		assertNull(column.getAlias());
+		assertEquals(SQLOperator.EQUAL, stmt.getOperator());
+		assertEquals(42, stmt.getValue());
+	}
+	
+	@Test
+	public void testMakeConditionalStatementColumnWithDot(){
+		ConditionalStatement stmt = SQLSyntaxUtil.makeConditionalStmt(true, "Test.Derp", 42);
+		ColumnRef column = stmt.getColumn();
+		assertEquals("Test", column.getTableName());
+		assertEquals("Derp", column.getColumnName());
+		assertNull(column.getAlias());
+		assertEquals(SQLOperator.EQUAL, stmt.getOperator());
+		assertEquals(42, stmt.getValue());
+	}
+	
+	@Test
+	public void testMakeConditionalStatementStringNotSearch(){
+		ConditionalStatement stmt = SQLSyntaxUtil.makeConditionalStmt(false, "Test", "Derp");
+		ColumnRef column = stmt.getColumn();
+		assertNull(column.getTableName());
+		assertEquals("Test", column.getColumnName());
+		assertNull(column.getAlias());
+		assertEquals(SQLOperator.EQUAL, stmt.getOperator());
+		assertEquals("Derp", stmt.getValue());
+	}
+	
+	@Test
+	public void testMakeConditionalStatementStringSearch(){
+		ConditionalStatement stmt = SQLSyntaxUtil.makeConditionalStmt(true, "Test", "Derp");
+		ColumnRef column = stmt.getColumn();
+		assertNull(column.getTableName());
+		assertEquals("Test", column.getColumnName());
+		assertNull(column.getAlias());
+		assertEquals(SQLOperator.LIKE, stmt.getOperator());
+		assertEquals("%Derp%", stmt.getValue());
+	}
+	
+	@Test
+	public void testFormatInsertStatement(){
+		String insertStmt = SQLSyntaxUtil.formatInsertStatement("Test", ListUtil.createList("Derp", "Plop"),
+				ListUtil.createList(42, true));
+		assertEquals("INSERT INTO Test (Derp, Plop) VALUES (42, true)", insertStmt);
 	}
 }
--- TadukooMySQL/src/com/github/tadukoo/database/mysql/syntax/SQLSyntaxUtil.java
+++ TadukooMySQL/src/com/github/tadukoo/database/mysql/syntax/SQLSyntaxUtil.java
@@ -1,5 +1,15 @@
 package com.github.tadukoo.database.mysql.syntax;
 
+import com.github.tadukoo.database.mysql.syntax.conditional.ConditionalStatement;
+import com.github.tadukoo.database.mysql.syntax.conditional.SQLOperator;
+import com.github.tadukoo.database.mysql.syntax.reference.ColumnRef;
+import com.github.tadukoo.database.mysql.syntax.reference.TableRef;
+import com.github.tadukoo.database.mysql.syntax.statement.SQLInsertStatement;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * SQL Syntax Util provides utilities for handling SQL syntax.
  *
@@ -27,5 +37,90 @@ public class SQLSyntaxUtil{
 		}else{
 			return value.toString();
 		}
+	}
+	
+	/**
+	 * Makes a single {@link ColumnRef} using the given columnName
+	 *
+	 * @param columnName The name of the column
+	 * @return A {@link ColumnRef} with the given columnName
+	 */
+	public static ColumnRef makeColumnRef(String columnName){
+		if(columnName.contains(".")){
+			String[] pieces = columnName.split("\\.");
+			return ColumnRef.builder()
+					.tableName(pieces[0])
+					.columnName(pieces[1])
+					.build();
+		}else{
+			return ColumnRef.builder()
+					.columnName(columnName)
+					.build();
+		}
+	}
+	
+	/**
+	 * Makes a List of {@link ColumnRef ColumnRefs} using the given columnNames
+	 *
+	 * @param columnNames The names of the columns
+	 * @return A List of {@link ColumnRef ColumnRefs} with the given columnNames
+	 */
+	public static List<ColumnRef> makeColumnRefs(Collection<String> columnNames){
+		return columnNames.stream()
+				.map(SQLSyntaxUtil::makeColumnRef)
+				.collect(Collectors.toList());
+	}
+	
+	/**
+	 * Creates a {@link ConditionalStatement} using the given parameters.
+	 * <br><br>
+	 * If search is {@code false}, it will create an equals statement with the given columnName and value
+	 * <br><br>
+	 * If search is {@code true}, it will create an equals statement for all non-string values. For String
+	 * values, it will create a LIKE statement and surround the value with % for matching strings with that
+	 * value anywhere in the string
+	 *
+	 * @param search Whether to consider this a search and do LIKE for strings, or not
+	 * @param columnName The name of the column
+	 * @param value The value for the conditional statement
+	 * @return A new {@link ConditionalStatement}
+	 */
+	public static ConditionalStatement makeConditionalStmt(boolean search, String columnName, Object value){
+		// Determine operator (search = true + String value will make it a LIKE with % around it)
+		SQLOperator operator = SQLOperator.EQUAL;
+		if(search && value instanceof String){
+			operator = SQLOperator.LIKE;
+			value = "%" + value + "%";
+		}
+		
+		// Make the statement and return it
+		return ConditionalStatement.builder()
+				.column(makeColumnRef(columnName))
+				.operator(operator)
+				.value(value)
+				.build();
+	}
+	
+	/**
+	 * Creates an Insert statement for the given parameters
+	 *
+	 * @param table The name of the table to insert into
+	 * @param columnNames The names of the columns to insert into
+	 * @param values The values to insert into the columns
+	 * @return The SQL text for the insert statement
+	 */
+	public static String formatInsertStatement(String table, Collection<String> columnNames, Collection<Object> values){
+		// Convert the columns to ColumnRefs
+		List<ColumnRef> columns = makeColumnRefs(columnNames);
+		
+		// Build the insert statement
+		SQLInsertStatement insertStmt = SQLInsertStatement.builder()
+				.table(TableRef.builder().tableName(table).build())
+				.columns(columns)
+				.values(values.toArray())
+				.build();
+		
+		// Return the insert statement string
+		return insertStmt.toString();
 	}
 }
